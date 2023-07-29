69a1237514bec41a3a7b8b4ec3ce2d3db2becd90
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-07-29 20:56:27 +0530

    1 file modified

--- class.ts
+++ class.ts
@@ -4,14 +4,17 @@ class Coder {
     private age: number,
     public readonly name: string,
     protected id: string | number,
-    public reviews: boolean
+    public reviews: boolean = true
   ) {
     this.age = age;
     this.name = name;
     this.id = id;
-    this.reviews = reviews;
+    //this.reviews = reviews;
   }
-  public getDetails(){
-   return `Your name is ${this.name}`
+  public getDetails() {
+    return `Your name is ${this.name} and ${this.reviews} basic ${this.age}`;
   }
 }
+const result = new Coder(34, 'israr', 'au0097');
+console.log(result.getDetails()); //Your name is israr and true basic 34
+console.log(result.name); //israr
