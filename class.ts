7f0466d16b47bf7b123a18a9a0572e12200eb81e
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-07-29 19:51:13 +0530

    1 file modified

--- class.ts
+++ class.ts
@@ -1,1 +1,17 @@
 console.log('this is class');
+class Coder {
+  constructor(
+    private age: number,
+    public readonly name: string,
+    protected id: string | number,
+    public reviews: boolean
+  ) {
+    this.age = age;
+    this.name = name;
+    this.id = id;
+    this.reviews = reviews;
+  }
+  public getDetails(){
+   return `Your name is ${this.name}`
+  }
+}
