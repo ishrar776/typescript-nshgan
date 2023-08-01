3d5f748778aa494b90619c108cecf5bbfe3c3de5
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-08-01 18:03:45 +0530

    1 file modified

--- class.ts
+++ class.ts
@@ -1,4 +1,10 @@
-console.log('this is class');
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+//console.log('this is class');
 class Coder {
   constructor(
     private age: number,
@@ -12,10 +18,26 @@ class Coder {
     //this.reviews = reviews;
   }
   public getDetails() {
-    return `Your name is ${this.name} and ${this.reviews} basic ${this.age}`;
+    return `Your name is ${this.name} and ${this.reviews} basic and age is ${this.age} and Id is ${this.id}`;
   }
 }
 const result = new Coder(34, 'israr', 'au0097');
-console.log(result.getDetails()); //Your name is israr and true basic 34
-console.log(result.name); //israr// private are only acess within block of class and
-//protected can acess via block and derived class
+console.log(result.getDetails()); //YYour name is israr and true basic and age is 34 and Id is au0097
+//console.log(result.id); //israr
+class SubDerived extends Coder {
+  constructor(
+    public city: string,
+    age: number,
+    name: string,
+    id: string | number
+  ) {
+    super(age, name, id);
+    this.city = 'lucknow';
+  }
+  public getCity() {
+    return `Your name is ${this.name} and ${this.reviews} basic and city is ${this.city} and Id is ${this.id}`;
+  }
+}
+const showcity = new SubDerived('lucknow', 45, 'israr', '9987ur'); //Your name is israr and true basic and city is lucknow and Id is 9987ur
+
+console.log(showcity.getCity());
