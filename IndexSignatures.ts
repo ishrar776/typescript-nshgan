b77b9699debfab71e27f671e2fadd676af7a681c
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-08-01 21:37:35 +0530

    1 file modified

--- IndexSignatures.ts
+++ IndexSignatures.ts
@@ -1,90 +1,49 @@
-//Index Signatures
-//Index Signatures
-// Index Signatures
-
-// interface TransactionObj {
-//     readonly [index: string]: number
-// }
 interface TransactionObj {
   readonly [index: string]: number;
   Pizza: number;
   Books: number;
   Job: number;
 }
-
 const todaysTransactions: TransactionObj = {
   Pizza: -10,
   Books: -5,
   Job: 50,
+  Security: 25,
 };
-
 console.log(todaysTransactions.Pizza);
 console.log(todaysTransactions['Pizza']);
-
-let prop: string = 'Pizza';
+let prop: string = 'Security';
 console.log(todaysTransactions[prop]);
 
-const todaysNet = (transactions: TransactionObj): number => {
-  let total = 0;
-  for (const transaction in transactions) {
-    total += transactions[transaction];
+const totalAmount = (traccation: TransactionObj): number => {
+  let exatAmount = 0;
+  for (const findTracation in traccation) {
+    exatAmount += traccation[findTracation];
   }
-  return total;
+  return exatAmount;
 };
+console.log(totalAmount(todaysTransactions));
 
-console.log(todaysNet(todaysTransactions));
-
-//todaysTransactions.Pizza = 40
-
-console.log(todaysTransactions['Dave']); // undefined
-
-///////////////////////////////////
-
+//other example
 interface Student {
-  //[key: string]: string | number | number[] | undefined
+  //[key: string]: string | number | number[] | undefined;
   name: string;
   GPA: number;
   classes?: number[];
 }
-
 const student: Student = {
   name: 'Doug',
   GPA: 3.5,
   classes: [100, 200],
 };
-
-// console.log(student.test)
-
+//console.log(student.test);
 for (const key in student) {
   console.log(`${key}: ${student[key as keyof Student]}`);
 }
-
 Object.keys(student).map((key) => {
   console.log(student[key as keyof typeof student]);
 });
-
 const logStudentKey = (student: Student, key: keyof Student): void => {
   console.log(`Student ${key}: ${student[key]}`);
 };
-
 logStudentKey(student, 'name');
-
-/////////////////////////////////
-
-// interface Incomes {
-//     [key: string]: number
-// }
-
-type Streams = 'salary' | 'bonus' | 'sidehustle';
-
-type Incomes = Record<Streams, number>;
-
-const monthlyIncomes: Incomes = {
-  salary: 500,
-  bonus: 100,
-  sidehustle: 250,
-};
-
-for (const revenue in monthlyIncomes) {
-  console.log(monthlyIncomes[revenue as keyof Incomes]);
-}
