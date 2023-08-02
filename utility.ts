1e7152c1384d1d1409ed30023cf1df696276ace1
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-08-02 17:16:49 +0530

    1 file modified

--- utility.ts
+++ utility.ts
@@ -17,9 +17,22 @@ const updateAssignment = (
   return { ...assign, ...updateData };
 };
 console.log(updateAssignment(detailsAssign, { grade: 478 }));
+const exactValue: Assignment = updateAssignment(detailsAssign, { grade: 478 });
 //Required
 const updateAssignmentRequired = (assign: Required<Assignment>): Assignment => {
   return assign;
 };
 console.log(updateAssignmentRequired({ ...detailsAssign, verified: true }));
 // if we do not provide second parameter then it will show error since it required all
+//Readonly
+const fixedAssesmentValue: Readonly<Assignment> = {
+  ...exactValue,
+  verified: true,
+};
+//Record
+// Record
+const hexColorMap: Record<string, string> = {
+  red: 'FF0000',
+  green: '00FF00',
+  blue: '0000FF',
+};
