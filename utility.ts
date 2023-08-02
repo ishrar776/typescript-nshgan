e1ab9668ed4f98487912fc122831c73acf9c7a48
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-08-02 16:11:47 +0530

    1 file added

---
+++ utility.ts
@@ -0,0 +1,25 @@
+//// Partial
+interface Assignment {
+  studentId: string;
+  title: string;
+  grade: number;
+  verified?: boolean;
+}
+const detailsAssign: Assignment = {
+  studentId: 'Umesh001',
+  title: 'Assignment Details',
+  grade: 657,
+};
+const updateAssignment = (
+  assign: Assignment,
+  updateData: Partial<Assignment>
+): Assignment => {
+  return { ...assign, ...updateData };
+};
+console.log(updateAssignment(detailsAssign, { grade: 478 }));
+//Required
+const updateAssignmentRequired = (assign: Required<Assignment>): Assignment => {
+  return assign;
+};
+console.log(updateAssignmentRequired({ ...detailsAssign, verified: true }));
+// if we do not provide second parameter then it will show error since it required all
